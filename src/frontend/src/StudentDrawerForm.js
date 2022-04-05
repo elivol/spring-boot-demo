e1933118a8e0ea5e4785a720fabf597fe05eb9a2
liza <elivolov@gmail.com>
2022-04-05 18:25:54 +0300

    added edit student function. need to fix frontend

--- src/frontend/src/StudentDrawerForm.js
+++ src/frontend/src/StudentDrawerForm.js
@@ -5,8 +5,65 @@ import {successNotification, errorNotification} from './Notification';
 
 const {Option} = Select;
 
-function StudentDrawerForm({showDrawer, setShowDrawer, fetchStudents}) {
-    const onCLose = () => setShowDrawer(false);
+function StudentForm(props) {
+    return (
+        <Form layout="vertical"
+              onFinishFailed={props.onFinishFailed}
+              onFinish={props.onFinish}
+              hideRequiredMark>
+            <Row gutter={16}>
+                <Col span={12}>
+                    <Form.Item
+                        name="name"
+                        label="Name"
+                        rules={[{required: true, message: 'Please enter student name'}]}
+                    >
+                        <Input placeholder="Please enter student name" defaultValue={props.student && props.student.name}/>
+                    </Form.Item>
+                </Col>
+                <Col span={12}>
+                    <Form.Item
+                        name="email"
+                        label="Email"
+                        rules={[{required: true, message: 'Please enter student email'}]}
+                    >
+                        <Input placeholder="Please enter student email" defaultValue={props.student && props.student.email}/>
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row gutter={16}>
+                <Col span={12}>
+                    <Form.Item
+                        name="gender"
+                        label="gender"
+                        rules={[{required: true, message: 'Please select a gender'}]}
+                    >
+                        <Select placeholder="Please select a gender" defaultValue={props.student && props.student.gender}>
+                            <Option value="MALE">MALE</Option>
+                            <Option value="FEMALE">FEMALE</Option>
+                            <Option value="OTHER">OTHER</Option>
+                        </Select>
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row>
+                <Col span={12}>
+                    <Form.Item >
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row>
+                {props.submitting && <Spin />}
+            </Row>
+        </Form>
+    );
+}
+
+function StudentDrawerForm(props) {
+    const onClose = () => props.setShowDrawer(false);
     const [submitting, setSubmitting] = useState(false);
 
     const onFinish = student => {
@@ -15,12 +72,12 @@ function StudentDrawerForm({showDrawer, setShowDrawer, fetchStudents}) {
         addNewStudent(student)
             .then(() => {
                 console.log("student added");
-                onCLose();
+                onClose();
                 successNotification(
                     "Student successfully added",
                     `${student.name} was added to the system`
                 )
-                fetchStudents();
+                props.fetchStudents();
             }).catch(err => {
                 console.log(err);
                 err.response.json().then(res => {
@@ -41,10 +98,10 @@ function StudentDrawerForm({showDrawer, setShowDrawer, fetchStudents}) {
     };
 
     return <Drawer
-        title="Create new student"
+        title={props.title}
         width={720}
-        onClose={onCLose}
-        visible={showDrawer}
+        onClose={onClose}
+        visible={props.showDrawer}
         bodyStyle={{paddingBottom: 80}}
         footer={
             <div
@@ -52,64 +109,18 @@ function StudentDrawerForm({showDrawer, setShowDrawer, fetchStudents}) {
                     textAlign: 'right',
                 }}
             >
-                <Button onClick={onCLose} style={{marginRight: 8}}>
+                <Button onClick={onClose} style={{marginRight: 8}}>
                     Cancel
                 </Button>
             </div>
         }
     >
-        <Form layout="vertical"
-              onFinishFailed={onFinishFailed}
-              onFinish={onFinish}
-              hideRequiredMark>
-            <Row gutter={16}>
-                <Col span={12}>
-                    <Form.Item
-                        name="name"
-                        label="Name"
-                        rules={[{required: true, message: 'Please enter student name'}]}
-                    >
-                        <Input placeholder="Please enter student name"/>
-                    </Form.Item>
-                </Col>
-                <Col span={12}>
-                    <Form.Item
-                        name="email"
-                        label="Email"
-                        rules={[{required: true, message: 'Please enter student email'}]}
-                    >
-                        <Input placeholder="Please enter student email"/>
-                    </Form.Item>
-                </Col>
-            </Row>
-            <Row gutter={16}>
-                <Col span={12}>
-                    <Form.Item
-                        name="gender"
-                        label="gender"
-                        rules={[{required: true, message: 'Please select a gender'}]}
-                    >
-                        <Select placeholder="Please select a gender">
-                            <Option value="MALE">MALE</Option>
-                            <Option value="FEMALE">FEMALE</Option>
-                            <Option value="OTHER">OTHER</Option>
-                        </Select>
-                    </Form.Item>
-                </Col>
-            </Row>
-            <Row>
-                <Col span={12}>
-                    <Form.Item >
-                        <Button type="primary" htmlType="submit">
-                            Submit
-                        </Button>
-                    </Form.Item>
-                </Col>
-            </Row>
-            <Row>
-                {submitting && <Spin />}
-            </Row>
-        </Form>
+        <StudentForm
+            onFinish = {onFinish}
+            onFinishFailed = {onFinishFailed}
+            submitting = {submitting}
+            student = {props.student}
+        />
     </Drawer>
 }
 
